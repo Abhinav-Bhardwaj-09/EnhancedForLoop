f544ec125fd611ba3654c3a3463ac7641af9478c
Abhinav Bhardwaj <abhinav.bhardwaj_cs18@gla.ac.in>
2021-02-27 19:12:46 +0530

    After defining all methods required to initialize array, insert elements and display array elements.

--- src/array_methods/Array_Methods.java
+++ src/array_methods/Array_Methods.java
@@ -8,4 +8,29 @@ package array_methods;
  */
 
 public class Array_Methods {
+    private int [] arr;
+    private int index = 0;
+    public void initialize_Array (int size) {
+        arr = new int[size];
+    }
+
+
+    public void insert_Element (int element) {
+        arr[index] = element;
+        index++;
+    }
+
+    public void display_Array () {
+        int i = 0;
+        for(int item : arr){         // enhanced for loop iterating array elements in "item" variable
+            if (i != 0) {
+                System.out.print(", " + item);
+            }
+            else {
+                System.out.print(item);
+                i++;
+            }
+
+        }
+    }
 }
